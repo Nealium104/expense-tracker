d3702dc30726c85856fd148b14152c12b9c55081
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-08-13 09:20:43 -0400

    added inputs and state changes to new inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 import "./App.css";
 
 export default function App() {
@@ -32,6 +33,7 @@ export default function App() {
 
   return (
     <>
+      <NewExpense />
       <Expenses expense={expense} />
     </>
   );
